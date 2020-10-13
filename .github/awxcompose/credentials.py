d966d837baea0ef906cfab25152289da2d4c852b
sean-m-sullivan <ssulliva@redhat.com>
2020-10-13 12:18:25 -0500

    reorg and awx

---
+++ .github/awxcompose/credentials.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ATOMIC_REQUESTS': True,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "awx",
+        'USER': "awx",
+        'PASSWORD': "awxpass",
+        'HOST': "postgres",
+        'PORT': "5432",
+    }
+}
+
+BROADCAST_WEBSOCKET_SECRET = "YWlhM3VFOGdKNjhHeWtDV0JEVWU="
