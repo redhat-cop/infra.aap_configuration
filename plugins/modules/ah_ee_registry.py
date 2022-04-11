064b098e6aa17f35f4e6c9d882a93d7248a831ce
tompage1994@hotmail.co.uk <tpage@redhat.com>
2022-04-11 14:10:20 +0100

    Change copyright info

--- plugins/modules/ah_ee_registry.py
+++ plugins/modules/ah_ee_registry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2021, Herve Quatremain <hquatrem@redhat.com>
+# Copyright: (c) 2021, Tom Page <@Tompage1994>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # You can consult the UI API documentation directly on a running private
