2247f25f4b87fe2fbce3dc690c8a583b56397de7
Sean Sullivan <ssulliva@redhat.com>
2023-02-14 11:27:09 -0500

    remove unused import module (#225)

--- plugins/module_utils/ah_module.py
+++ plugins/module_utils/ah_module.py
@@ -15,7 +15,6 @@ from ansible.module_utils.six.moves.urllib.parse import urlparse, urlencode
 from ansible.module_utils.six.moves.urllib.error import HTTPError
 from ansible.module_utils.six.moves.http_cookiejar import CookieJar
 from ansible.module_utils._text import to_bytes, to_native, to_text
-from ansible.module_utils.compat.importlib import import_module  # noqa F401
 import os.path
 from socket import gethostbyname
 import re
