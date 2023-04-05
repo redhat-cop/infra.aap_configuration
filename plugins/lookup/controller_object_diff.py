f9d5456685fdf0de8588d41d8a5d29f9af4c6051
Ivan Aragonés Muniesa <26822043+ivarmu@users.noreply.github.com>
2023-04-05 17:02:19 +0200

    fix on map_item function

--- plugins/lookup/controller_object_diff.py
+++ plugins/lookup/controller_object_diff.py
@@ -115,9 +115,9 @@ class LookupModule(LookupBase):
 
         return compare_list
 
-    def map_item(self, item, new_attribute_name, attribute_name, dupitems):
+    def map_item(self, item, new_attribute_name, attribute_value, dupitems):
         new_item = copy.deepcopy(item)
-        new_item.update({new_attribute_name: item[attribute_name]})
+        new_item.update({new_attribute_name: attribute_value})
         for dupitem in [dupitem for dupitem in dupitems if dupitem in new_item]:
             new_item.pop(dupitem)
         return new_item
