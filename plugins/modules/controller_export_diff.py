23e609ca729ac07ef3de73a857cf90bd7c5b0ec3
Tom Page <tpage@redhat.com>
2023-08-16 14:12:39 +0100

    Fix broken CI because of request_timeout added to awx (#675)

--- plugins/modules/controller_export_diff.py
+++ plugins/modules/controller_export_diff.py
@@ -138,7 +138,6 @@ options:
       - A dictionary structure as returned by the token module.
       - If value not set, will try environment variable C(CONTROLLER_OAUTH_TOKEN) and then config files
       type: raw
-      version_added: "3.7.0"
       aliases: [ tower_oauthtoken ]
     validate_certs:
       description:
@@ -148,6 +147,12 @@ options:
       - If value not set, will try environment variable C(CONTROLLER_VERIFY_SSL) and then config files
       type: bool
       aliases: [ tower_verify_ssl ]
+    request_timeout:
+      description:
+      - Specify the timeout Ansible should use in requests to the controller host.
+      - Defaults to 10s, but this is handled by the shared module_utils code
+      type: float
+      version_added: "2.6.0"
     controller_config_file:
       description:
       - Path to the controller config file.
